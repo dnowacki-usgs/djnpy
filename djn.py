7f6efc42f1a8e12ec5701e976bead9160c7ff559
Dan Nowacki <dnowacki@usgs.gov>
2017-04-05 17:45:51 -0400

    Add a nice font size helper

--- djn.py
+++ djn.py
@@ -17,4 +17,17 @@ def middles(edges):
     diffs = np.median(np.diff(edges));
     edgestart = edges[0] + diffs/2.;
     edgeend = edges[-1] - diffs/2.;
-    mid = np.arange(edgestart, edgeend, diffs)
+    mid = np.arange(edgestart, edgeend + diffs, diffs)
+
+    return mid
+
+def set_fontsize(fig,fontsize):
+    """
+    For each text object of a figure fig, set the font size to fontsize
+    http://stackoverflow.com/questions/7082597/in-matplotlib-how-do-you-change-the-fontsize-of-a-single-figure
+    """
+    def match(artist):
+        return artist.__module__ == "matplotlib.text"
+
+    for textobj in fig.findobj(match=match):
+        textobj.set_fontsize(fontsize)
