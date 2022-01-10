fcd3c1d1cbd70f6a48291d9daaff53718318a1ea
Dan Nowacki <dnowacki@usgs.gov>
2022-01-10 15:24:46 -0800

    Add Haversine distance function

--- djn.py
+++ djn.py
@@ -406,6 +406,24 @@ def make_log_smear_fit(xs, lr, smear):
     return np.exp(lr.slope * np.log(xs) + lr.intercept) * smear
 
 
+def haversine(lon1, lat1, lon2, lat2):
+    # https://stackoverflow.com/a/4913653
+    """
+    Calculate the great circle distance in kilometers between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = np.sin(dlat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2) ** 2
+    c = 2 * np.arcsin(np.sqrt(a))
+    r = 6371  # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
+
+
 def argmaxn(arr, n):
     # find largest N values from array
     return np.argpartition(arr, -n)[-n:]
